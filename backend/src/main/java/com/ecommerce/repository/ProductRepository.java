0cf82f9970f56a03da599b0b1eb59ec59ca5eae0
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-10-20 19:47:13 +0530

    dao layer in progress

--- backend/src/main/java/com/ecommerce/repository/ProductRepository.java
+++ backend/src/main/java/com/ecommerce/repository/ProductRepository.java
@@ -10,14 +10,21 @@ import com.ecommerce.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	@Query("select p from Product p" + "where (p.category.name=:category or :category='')" +
-			"and ((:minPriceis null and :maxPrice is null) or (p.discountedPrice between :minPrice and :maxPrice))" +
-			"and (:minDiscount is null or p.discountPercent >= :minDiscount)" +
-			"order by" + 
-			"case when :sort='price_low' then p.discountedPrice end asc," +
-			"case when :sort='price_high' then p.discountedPrice end desc")
-	public List<Product> filterProducts(@Param("category") String category,
-			@Param("minPrice") Integer minPrice, @Param("maxPrice") Integer maxPrice,
-			@Param("minDiscount") Integer minDiscount,@Param("sort") String sort);
+	@Query("SELECT p FROM Product p " +
+		       "WHERE (p.category.name = :category OR :category = '') " +
+		       "AND ((:minPrice IS NULL AND :maxPrice IS NULL) OR (p.discountedPrice BETWEEN :minPrice AND :maxPrice)) " +
+		       "AND (:minDiscount IS NULL OR p.discountPercent >= :minDiscount) " +
+		       "ORDER BY " +
+		       "CASE WHEN :sort = 'price_low' THEN p.discountedPrice END ASC, " +
+		       "CASE WHEN :sort = 'price_high' THEN p.discountedPrice END DESC")
+		public List<Product> filterProducts(@Param("category") String category,
+		                                     @Param("minPrice") Integer minPrice,
+		                                     @Param("maxPrice") Integer maxPrice,
+		                                     @Param("minDiscount") Integer minDiscount,
+		                                     @Param("sort") String sort);
+	
+	
+
+
 
 }
