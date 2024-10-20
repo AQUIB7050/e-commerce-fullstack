0dd25dfacf351fac9aaf5786477b726e0b5a7cd4
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-10-20 23:22:22 +0530

    business logic & api endpoints added

---
+++ backend/src/main/java/com/ecommerce/service/CartServiceImplementation.java
@@ -0,0 +1,77 @@
+package com.ecommerce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.exception.ProductException;
+import com.ecommerce.model.Cart;
+import com.ecommerce.model.CartItem;
+import com.ecommerce.model.Product;
+import com.ecommerce.model.User;
+import com.ecommerce.repository.CartRepository;
+import com.ecommerce.requests.AddItemRequest;
+
+@Service
+public class CartServiceImplementation implements CartService {
+	
+	@Autowired
+	private CartRepository cartRepository;
+	@Autowired
+	private CartItemService cartItemService;
+	@Autowired
+	private ProductService productService;
+	
+
+	@Override
+	public Cart createCart(User user) {
+		
+		Cart cart = new Cart();
+		cart.setUser(user);
+		return cartRepository.save(cart);
+	}
+
+	@Override
+	public String addCartItem(Long userId, AddItemRequest req) throws ProductException {
+		Cart cart = cartRepository.findByUserId(userId);
+		Product product = productService.findProductById(req.getProductId());
+		
+		CartItem isPresent = cartItemService.isCartItemExists(cart, product, req.getSize(), userId);
+		
+		if(isPresent == null) {
+			CartItem cartItem = new CartItem();
+			cartItem.setProduct(product);
+			cartItem.setCart(cart);
+			cartItem.setQuantity(req.getQuantity());
+			cartItem.setUserId(userId);
+			
+			int price = req.getQuantity()*product.getDiscountedPrice();
+			cartItem.setPrice(price);
+			cartItem.setSize(req.getSize());
+			
+			CartItem createdCartItem = cartItemService.createCartItem(cartItem);
+			cart.getCartItems().add(createdCartItem);
+		}
+		return "Item Added to cart";
+	}
+
+	@Override
+	public Cart findUserCart(Long userId) {
+		
+		Cart cart = cartRepository.findByUserId(userId);
+		int totalPrice=0, totalDiscountedPrice=0, totalItem=0;
+		
+		for(CartItem cartItem : cart.getCartItems()) {
+			totalPrice = totalPrice + cartItem.getPrice();
+			totalDiscountedPrice = totalDiscountedPrice + cartItem.getDiscountedPrice();
+			totalItem = totalItem+cartItem.getQuantity();
+		}
+		
+		cart.setTotalDiscountedPrice(totalDiscountedPrice);
+		cart.setTotalItem(totalItem);
+		cart.setTotalPrice(totalPrice);
+		cart.setDiscount(totalPrice-totalDiscountedPrice);
+		
+		return cartRepository.save(cart);
+	}
+
+}
